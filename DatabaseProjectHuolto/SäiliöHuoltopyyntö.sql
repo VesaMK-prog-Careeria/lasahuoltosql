160ffd45dfd1a957cd8a806c984854ef6012bcd9
Tiina Siremaa <tiinatuuliak@gmail.com>
2024-03-22 12:49:11 +0200

    muokkasin taulua

--- DatabaseProjectHuolto/SäiliöHuoltopyyntö.sql
+++ DatabaseProjectHuolto/SäiliöHuoltopyyntö.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[SäiliöHuoltopyyntö] (
-    [HuoltoID]         INT           NOT NULL PRIMARY KEY,
-    [SäiliöID]         INT           NULL,
-    [HuollonKuvaus]    NVARCHAR(2000) NULL,
-    [Kuva]             IMAGE         NULL,
+    [HuoltoID]      INT             NOT NULL PRIMARY KEY CLUSTERED ([HuoltoID] ASC),
+    [SäiliöID]      INT             NOT NULL,
+    [HuollonKuvaus] NVARCHAR (2000) NULL,
+    [Kuva]          IMAGE           NULL,
     CONSTRAINT [FK_SäiliöHuoltopyyntö_Säiliö] FOREIGN KEY ([SäiliöID]) REFERENCES [dbo].[Säiliö] ([SäiliöID])
 );
 
