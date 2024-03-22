5b24484f9b3d04e8cbb0ea61f9ba7286ddfb87c7
Tiina Siremaa <tiinatuuliak@gmail.com>
2024-03-22 11:41:01 +0200

    muokattu image nimeä

--- DatabaseProjectHuolto/PvHuollot.sql
+++ DatabaseProjectHuolto/PvHuollot.sql
@@ -1,10 +1,10 @@
 CREATE TABLE [dbo].[PvHuollot] (
-    [HuollonID]         INT           NOT NULL PRIMARY KEY,
-    [PvID]              INT           NULL,
-    [HuoltoPvm]         DATE          NULL,
-    [Huoltopaikka]      NVARCHAR(50)  NULL,
-    [HuollonKuvaus]     NVARCHAR(2000) NULL,
-    [Image]             VARBINARY(MAX) NULL,
-    CONSTRAINT [FK_PvHuollot_Pv] FOREIGN KEY ([PvID]) REFERENCES [dbo].[Pv] ([PvID]),
-    CONSTRAINT [FK_PvHuollot_Huoltopaikat] FOREIGN KEY ([Huoltopaikka]) REFERENCES [dbo].[Huoltopaikat] ([HuoltoPaikka])
+    [HuollonID]     INT             NOT NULL PRIMARY KEY CLUSTERED ([HuollonID] ASC),
+    [PvID]          INT             NULL,
+    [HuoltoPvm]     DATE            NULL,
+    [Huoltopaikka]  NVARCHAR (50)   NULL,
+    [HuollonKuvaus] NVARCHAR (2000) NULL,
+    [Kuva]          IMAGE           NULL,
+    CONSTRAINT [FK_PvHuollot_Huoltopaikat] FOREIGN KEY ([Huoltopaikka]) REFERENCES [dbo].[Huoltopaikat] ([Huoltopaikka]),
+    CONSTRAINT [FK_PvHuollot_Pv] FOREIGN KEY ([PvID]) REFERENCES [dbo].[PV] ([PvID])
 );
--- DatabaseProjectHuolto/SäiliöHuollot.sql
+++ DatabaseProjectHuolto/SäiliöHuollot.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[SäiliöHuollot] (
     [HuoltoPVM]      DATE           NULL,
     [HuoltoPaikkaID] NCHAR (50)     NULL,
     [HuollonKuvaus]  NVARCHAR (200) NULL,
-    [Image]          IMAGE          NULL,
+    [Kuva]           IMAGE          NULL,
     CONSTRAINT [FK_SäiliöHuollot_HuoltoPaikka] FOREIGN KEY ([HuoltoPaikkaID]) REFERENCES [dbo].[Huoltopaikat] ([HuoltoPaikkaID]),
     CONSTRAINT [FK_SäiliöHuollot_Säiliö] FOREIGN KEY ([SäiliöID]) REFERENCES [dbo].[Säiliö] ([SäiliöID])
 );
